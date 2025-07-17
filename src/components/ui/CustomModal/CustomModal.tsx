15ecccd9a76625f8bef784f8b9b4e166a29bb4ec
seunghyeon-orheunil <shoh@orheun.com>
2025-07-17 10:12:05 +0900

    feat: 모달 공통 컴포넌트

---
+++ src/components/ui/CustomModal/CustomModal.tsx
@@ -0,0 +1,67 @@
+import {
+  Modal,
+  Pressable,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { CustomText } from "../CustomText";
+
+interface CustomModalProps {
+  visible: boolean;
+  message: string;
+  onClose: () => void;
+}
+
+export const CustomModal = ({
+  visible,
+  onClose,
+  message,
+}: CustomModalProps) => {
+  return (
+    <Modal transparent={true} visible={visible}>
+      <Pressable onPress={onClose} style={styles.overlay}>
+        <TouchableWithoutFeedback onPress={() => {}}>
+          <View style={styles.modalContainer}>
+            <CustomText fontSize={18} fontWeight="600">
+              {message}
+            </CustomText>
+            <Pressable onPress={onClose} style={styles.closeButton}>
+              <CustomText fontSize={16} fontWeight="500" color="#fff">
+                확인
+              </CustomText>
+            </Pressable>
+          </View>
+        </TouchableWithoutFeedback>
+      </Pressable>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    width: 280,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    paddingVertical: 24,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  closeButton: {
+    marginTop: 24,
+    backgroundColor: "#007AFF",
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    borderRadius: 8,
+  },
+});
