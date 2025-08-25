a7858ac1afb01f195887026c8b0d7957d7473c71
seunghyeon-orheunil <shoh@orheun.com>
2025-08-25 18:22:32 +0900

    feat: Response 인터셉터

--- src/libs/custom-instance.ts
+++ src/libs/custom-instance.ts
@@ -2,12 +2,56 @@ import Axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import * as SecureStore from "expo-secure-store";
 import CookieManager from "@react-native-cookies/cookies";
 
+interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
+  _retry?: boolean;
+}
+
 export const AXIOS_INSTANCE = Axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
   withCredentials: true,
 });
 
-// API 요청 시 SecureStorage에서 토큰을 꺼내 쿠키에 담아 전송
+// 엑세스 토큰 재발급
+const getNewAccessToken = async (): Promise<void> => {
+  const refreshToken = await SecureStore.getItemAsync("refreshToken");
+
+  if (!refreshToken) {
+    throw {
+      message: "로그인이 만료되었습니다.",
+      status: 401,
+    };
+  }
+
+  const response = await AXIOS_INSTANCE.post(
+    "/token/refresh",
+    {},
+    {
+      headers: { Cookie: `refreshToken=${refreshToken}` },
+    }
+  );
+
+  const newAccessToken = response.data.accessToken;
+  const newRefreshToken = response.data.refreshToken;
+
+  // SecureStore 갱신
+  await SecureStore.setItemAsync("accessToken", newAccessToken);
+  await SecureStore.setItemAsync("refreshToken", newRefreshToken);
+
+  // 쿠키 갱신
+  await CookieManager.set(process.env.EXPO_PUBLIC_API_URL!, {
+    name: "accessToken",
+    value: newAccessToken,
+    path: "/",
+  });
+
+  await CookieManager.set(process.env.EXPO_PUBLIC_API_URL!, {
+    name: "refreshToken",
+    value: newRefreshToken,
+    path: "/",
+  });
+};
+
+// Request 인터셉터
 AXIOS_INSTANCE.interceptors.request.use(
   async (config) => {
     const accessToken = await SecureStore.getItemAsync("accessToken");
@@ -32,6 +76,34 @@ AXIOS_INSTANCE.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+// Response 인터셉터
+AXIOS_INSTANCE.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as AxiosRequestConfigWithRetry;
+
+    if (!originalRequest) {
+      return Promise.reject(error);
+    }
+
+    // 401 && 아직 재시도 안 한 요청
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      // 엑세스 토큰 재발급 요청
+      try {
+        await getNewAccessToken();
+        // 새로운 토큰으로 원래 요청 재시도
+        return AXIOS_INSTANCE(originalRequest);
+      } catch (error) {
+        throw error;
+      }
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export const customInstance = <T = any>(
