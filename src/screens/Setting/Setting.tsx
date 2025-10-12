ae0d64785610e0819e7469d88e047508e001486e
seunghyeon-orheunil <shoh@orheun.com>
2025-10-12 23:25:24 +0900

    feat: 앱 버전 업데이트 기능

--- src/screens/Setting/Setting.tsx
+++ src/screens/Setting/Setting.tsx
@@ -5,11 +5,15 @@ import { colors } from "@/styles";
 import { getResponsiveSize } from "@/utils";
 import { CommonActions, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Image, Platform, Pressable, StyleSheet, View } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import CookieManager from "@react-native-cookies/cookies";
+import * as Linking from "expo-linking";
 import { useState } from "react";
 import { CustomModal } from "@/components/ui/CustomModal";
+import { rigthArrowIcon } from "@/assets/images";
+import { useSetAtom } from "jotai";
+import { errorModalAtom } from "@/recoil";
 
 export const Setting = () => {
   const containerNavigation =
@@ -18,11 +22,32 @@ export const Setting = () => {
   const settingNavigation =
     useNavigation<NativeStackNavigationProp<SettingStackParamList>>();
 
+  const setErrorModal = useSetAtom(errorModalAtom);
+
   const [showModal, setShowModal] = useState(false);
 
+  // 로그아웃 API
   const { mutateAsync: logout, isPending: logoutLoading } =
     useAuthControllerLogout();
 
+  // 앱 버전 업데이트
+  const handleOpenStore = async () => {
+    const storeUrl =
+      Platform.OS === "ios"
+        ? "https://apps.apple.com/app/id6467127880"
+        : "https://play.google.com/store/apps/details?id=io.allta.user";
+
+    try {
+      await Linking.openURL(storeUrl);
+    } catch (error) {
+      setErrorModal({
+        visible: true,
+        message: "앱스토어 이동에 실패했습니다.",
+      });
+    }
+  };
+
+  // 로그아웃
   const handleLogout = async () => {
     try {
       await logout();
@@ -71,6 +96,7 @@ export const Setting = () => {
         style={styles.button}
       >
         <CustomText fontSize={16}>알림 설정</CustomText>
+        <Image source={rigthArrowIcon} style={styles.icon} />
       </Pressable>
 
       <Pressable
@@ -80,9 +106,15 @@ export const Setting = () => {
         <CustomText fontSize={16}>약관 및 정책</CustomText>
       </Pressable>
 
-      <Pressable style={styles.button}>
+      <View style={styles.button}>
         <CustomText fontSize={16}>버전 정보</CustomText>
-      </Pressable>
+
+        <Pressable onPress={handleOpenStore} style={styles.updateButton}>
+          <CustomText color={colors.gray7} fontSize={12} fontWeight={"500"}>
+            업데이트
+          </CustomText>
+        </Pressable>
+      </View>
 
       <Pressable onPress={() => setShowModal(true)} style={styles.button}>
         <CustomText fontSize={16}>로그아웃</CustomText>
@@ -113,5 +145,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     paddingVertical: getResponsiveSize(12),
+  },
+  icon: {
+    width: getResponsiveSize(24),
+    height: getResponsiveSize(24),
+  },
+  updateButton: {
+    paddingVertical: getResponsiveSize(6),
+    paddingHorizontal: getResponsiveSize(10),
+    borderWidth: 1,
+    borderColor: colors.line,
+    borderRadius: 8,
   },
 });
