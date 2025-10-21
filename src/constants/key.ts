a2270803a55bcf82e5ef279e6babcbc7a1f7ae31
seunghyeon-orheunil <shoh@orheun.com>
2025-10-21 16:04:40 +0900

    refactor: 커밋 병합

--- src/constants/key.ts
+++ src/constants/key.ts
@@ -1,5 +1,5 @@
+// 첫 실행 여부
 export const IS_FIRST_LAUCH = "app.isFirstLaunch";
-export const POPUP_CLOSE_DATE = "app.popupCloseDate";
 
 // 알림 수신 여부
 export const IS_NOTIFICATION_GRANTED = "app.isNotificationGranted";
